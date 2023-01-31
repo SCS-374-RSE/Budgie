98d8d4cefb8c80272968b2b24e25e2acec2b7d15
Caleb Voorhees <c.voorhees@setonhill.edu>
2023-01-30 20:53:57 -0500

    Initial commit for Budgie project

---
+++ Budgie/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Budgie;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Budgie/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Budgie;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
