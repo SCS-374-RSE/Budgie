5f8cd3080e258016ca39f6499c9c55bbd77df8b7
Caleb Voorhees <c.voorhees@setonhill.edu>
2023-03-13 13:26:00 -0400

    Addition of view model for create page
    - Deleted unused files
    - Slight changes to create page front end and code behind to connect them to the view model

---
+++ Budgie/CreatePageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+namespace Budgie
+{
+    public class CreatePageViewModel : INotifyPropertyChanged
+    {
+        public CreatePageViewModel()
+        {
+            SubmitFormCommand = new Command(OnFormSubmission);
+        }
+
+        public ICommand SubmitFormCommand { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string budgetName { get; set; }
+        public int takeHomeAmt { get; set; }
+        public string budgetType { get; set; }
+
+        public string expenseTypeOne { get; set; }
+        public string expenseTypeTwo { get; set; }
+        public string expenseTypeThree { get; set; }
+        public string expenseTypeFour { get; set; }
+        public string expenseTypeFive { get; set; }
+
+        public int expenseCostOne { get; set; }
+        public int expenseCostTwo { get; set; }
+        public int expenseCostThree { get; set; }
+        public int expenseCostFour { get; set; }
+        public int expenseCostFive { get; set; }
+
+        public void OnFormSubmission()
+        {
+            budgetName.ToLower();
+        }
+    }
+}
+
